eaf6c7390c83c9cb38e41692fc04d6416c05f153
Onkar Basu <150782284+OnkarBasu@users.noreply.github.com>
2025-07-18 13:42:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,13 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
 import pickle
+import os
 
-
-
-# Step 1: Set up the Flask app and enable CORS
 app = Flask(__name__)
 CORS(app)
 
 # Step 2: Load both models
-with open('C:/Users/abasu/OneDrive/Desktop/backend1/model_pipeline.pkl', 'rb') as f:
+with open('model_pipeline.pkl', 'rb') as f:
     car_model_pipeline = pickle.load(f)
 
 with open('model_pipelinebike.pkl', 'rb') as f:
@@ -32,8 +30,8 @@ def predict_bike():
     prediction = bike_model_pipeline.predict(input_df)[0]
     return jsonify({'predicted_price': round(prediction, 2)})
 
-# Step 5: Run the app
-if __name__ == '__main__':
+# Don't include app.run() — gunicorn will handle it
+
     app.run(debug=True, host='0.0.0.0', port=5000)
 
 
